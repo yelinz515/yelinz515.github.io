d4e983a683d6f1bd00117e14016c397e27368c0d
yelin <ggg06141@gmail.com>
2025-04-13 15:43:33 +0900

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomePage({ data }) {
   const posts = data.allMarkdownRemark.edges.map(({ node }) => new Post(node));
   const { author, language } = data.site.siteMetadata;
   const categories = ['All', ...getUniqueCategories(posts)];
-  const featuredTabIndex = categories.findIndex((category) => category === 'All'); //'featured'
+  const featuredTabIndex = categories.findIndex((category) => category === 'blog'); //'featured'
   const [tabIndex, setTabIndex] = useState(featuredTabIndex === -1 ? 0 : featuredTabIndex);
   const onTabIndexChange = useCallback((e, value) => setTabIndex(value), []);
 
